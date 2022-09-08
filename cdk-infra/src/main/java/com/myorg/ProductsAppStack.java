adba4e497a34cb1d5d0baa7c720178b661df436f
Rodrigo R Santos <rodrigorochasantos@outlook.com>
2022-09-08 08:57:07 -0300

    Inicia construcao da lambda de buscar produtos

---
+++ cdk-infra/src/main/java/com/myorg/ProductsAppStack.java
@@ -0,0 +1,31 @@
+package com.myorg;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.constructs.Construct;
+
+public class ProductsAppStack extends Stack {
+
+    private Function productsFetchHandler;
+
+    public ProductsAppStack(Construct scope, String stackId) {
+        super(scope, stackId, null);
+
+        this.productsFetchHandler = createProductsFetchLambda();
+    }
+
+    private Function createProductsFetchLambda() {
+        return Function.Builder.create(this, "ProductsFetchLambda")
+                .functionName("ProductsFetchLambda")
+                .handler("products.ProductsFetchLambda") /* igual ao do projeto products-lambda. É permitido referenciar
+                                                            o pacote.nome_da_classe pq esta implementa a interface RequestHandler */
+                .memorySize(512)
+                .timeout(Duration.seconds(3))
+                .code(Code.fromAsset(""))
+                .runtime(Runtime.JAVA_11)
+                .build();
+    }
+}
