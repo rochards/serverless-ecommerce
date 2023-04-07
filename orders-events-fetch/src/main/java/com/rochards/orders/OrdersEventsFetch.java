cdd68521149940e9690000072e512c5a7aa27e69
Rocha <rodrigorochasantos@outlook.com>
2023-04-07 20:41:27 -0300

    inicia construcao de orders-events-fetch

---
+++ orders-events-fetch/src/main/java/com/rochards/orders/OrdersEventsFetch.java
@@ -0,0 +1,13 @@
+package com.rochards.orders;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class OrdersEventsFetch implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+        return null;
+    }
+}
