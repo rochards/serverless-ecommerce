669c31d3fb6ed90d426c82b60b0c6d8a90c29559
Rocha <rodrigorochasantos@outlook.com>
2023-06-03 14:16:04 -0300

    Add classe InvoiceApiStack
    - Cria tabela do dynamoDB.

--- cdk-infra/src/main/java/com/myorg/InvoiceApiStack.java
+++ cdk-infra/src/main/java/com/myorg/InvoiceApiStack.java
@@ -1,2 +1,41 @@
-package com.myorg;public class InvoiceApiStack {
+package com.myorg;
+
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.constructs.Construct;
+
+public class InvoiceApiStack extends Stack {
+
+    public InvoiceApiStack(Construct scope, String stackId) {
+        super(scope, stackId, null);
+
+        Table invoicesAndTransactionsDdb = createDynamoDBTable();
+    }
+
+    private Table createDynamoDBTable() {
+        return Table.Builder.create(this, "InvoicesAndTransactionsTable")
+                .tableName("InvoicesAndTransactionsTable")
+                .billingMode(BillingMode.PROVISIONED)
+                .readCapacity(3)
+                .writeCapacity(3)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .partitionKey(
+                        Attribute.builder()
+                                .name("Pk")
+                                .type(AttributeType.STRING)
+                                .build()
+                )
+                .sortKey(
+                        Attribute.builder()
+                                .name("Sk")
+                                .type(AttributeType.STRING)
+                                .build()
+                )
+                .timeToLiveAttribute("Ttl")
+                .build();
+    }
 }
