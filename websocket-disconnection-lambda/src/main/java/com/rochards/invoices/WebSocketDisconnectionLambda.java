784850e2e9de59191fbc15e4c8d4012bc8798abe
Rocha <rodrigorochasantos@outlook.com>
2023-06-17 18:43:45 -0300

    Alteracoes nas lambdas do websocket
    - Modifica classe do apigateway para lidar com os eventos do websocket
      nos projetos websocket-connection-lambda e
    websocket-disconnection-lambda.

--- websocket-disconnection-lambda/src/main/java/com/rochards/invoices/WebSocketDisconnectionLambda.java
+++ websocket-disconnection-lambda/src/main/java/com/rochards/invoices/WebSocketDisconnectionLambda.java
@@ -2,27 +2,21 @@ package com.rochards.invoices;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2WebSocketEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2WebSocketResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Map;
-
-public class WebSocketDisconnectionLambda implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class WebSocketDisconnectionLambda implements RequestHandler<APIGatewayV2WebSocketEvent, APIGatewayV2WebSocketResponse> {
 
     private static final Logger LOGGER = LogManager.getLogger(WebSocketDisconnectionLambda.class);
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+    public APIGatewayV2WebSocketResponse handleRequest(APIGatewayV2WebSocketEvent input, Context context) {
         LOGGER.info(input);
 
-        var response = new APIGatewayProxyResponseEvent();
+        var response = new APIGatewayV2WebSocketResponse();
         response.setStatusCode(200);
-        response.setHeaders(Map.of("Content-Type", "application/json"));
-        response.setBody(GSON.toJson(Map.of("message", "Ok")));
         return response;
     }
 }
